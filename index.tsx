39a37026c52a2af1e2185e91bc0756237f5fedd0
Jack Evans <jackevans38@gmail.com>
2021-03-08 11:35:52 +0000

    init commit

---
+++ index.tsx
@@ -0,0 +1,8 @@
+import { h, render } from "preact"
+import App from "./src/App"
+
+import "./reset.css"
+
+const root = document.getElementById("root")
+
+render(<App />, root!)
---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { h } from "preact"
+import styled from "@emotion/styled"
+import { Helmet } from "react-helmet"
+
+const PROJECT_SETTINGS = {
+	name: "Name",
+	meta: "Desc"
+}
+
+const Container = styled.div`
+	background-color: pink;
+`
+
+function App() {
+	return (
+		<Container>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<title>{PROJECT_SETTINGS.name}</title>
+				<meta name="description" content={PROJECT_SETTINGS.meta} />
+			</Helmet>
+
+			<h1>Hello theres world</h1>
+		</Container>
+	)
+}
+
+export default App
